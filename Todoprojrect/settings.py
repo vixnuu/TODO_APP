cdae9f53dc3374acff4f78e64a259b20f05aef6f
vishnu <vishnu@gmail.com>
2025-11-08 14:46:27 +0530

    AAA

--- Todoprojrect/settings.py
+++ Todoprojrect/settings.py
@@ -87,7 +87,7 @@ ASGI_APPLICATION = 'Todoprojrect.asgi.application'
 # }
 DATABASES = {
     'default': dj_database_url.config(
-        default='django.db.backends.sqlite3',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
